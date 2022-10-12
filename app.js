edd889d1e4260e20efc68fb561ee7f1d12835c16
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-10-13 00:33:18 +0600

    Database connecton successfull

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express")
 const app = express()
 
+// DB Livery Import 
+const mongoose = require("mongoose")
 
-
-
+// MongoDb Connection 
+const db_name = "blog_api_api";
+const OPTION = { user: "", pass: "", autoIndex: true }
+const URI = `mongodb://localhost:27017/${db_name}`
+mongoose.connect(URI, OPTION, (error)=>{
+    console.log(`DB Connection Success Full ! Your DB Name ${db_name} 😸 Your DB URI ${URI}`);
+    console.log(error)
+    
+})
 
 module.exports = app;
